13bebf031344db2ae56c4a68f3af74608a65796d
guswls6212 <guswls6212@gmail.com>
2020-05-19 22:56:37 +0900

    POST /api/events returns 201

--- src/main/java/me/hyulee/demo/events/Event.java
+++ src/main/java/me/hyulee/demo/events/Event.java
@@ -2,12 +2,15 @@ package me.hyulee.demo.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = {"id", "name"})
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -21,5 +24,6 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 }
---
+++ src/test/java/me/hyulee/demo/events/EventControllerTest.java
@@ -0,0 +1,62 @@
+package me.hyulee.demo.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
+    @Test
+    public void createEvent() throws Exception {
+        Event event = Event.builder()
+                    .name("Spring")
+                    .description("REST API Development with Spring")
+                    .beginEnrollmentDateTime(LocalDateTime.of(2020, 05, 19,0, 7))
+                    .closeEnrollmentDateTime(LocalDateTime.of(2020, 05, 20,0, 7))
+                    .beginEventDateTime(LocalDateTime.of(2020, 05, 21,0, 7))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 05, 22,0, 7))
+                    .basePrice(100)
+                    .maxPrice(200)
+                    .limitOfEnrollment(100)
+                    .location("도쿄 키타구 오우지 카미야")
+                    .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                    .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+        ;
+    }
+
+}
